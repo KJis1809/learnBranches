bbf26748b347941d8223e8e70367efe528053704
Kostya Gorbachev <gorbachev1809@gmail.com>
2021-02-09 20:42:11 +0500

    Remerge person2 (1st merge was unsaved)

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //add new Person
-<<<<<<< HEAD
         Person person1;
-=======
         Person person2;
->>>>>>> person2
     }
 }
